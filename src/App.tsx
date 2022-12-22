03092167ca1517cd2ac22328c7a83dabaa41b1b7
cryeprecision <muench.rico@gmail.com>
2022-12-22 18:07:44 +0100

    Add `react-latex` library

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <div className='App'>
       <h1>recard</h1>
-      <Recard question='Hello' answer='World' />
+      <Recard question='What is the meaning of life?' answer='$\int_a^b x dx = 42$' />
     </div>
   )
 }
--- src/components/Recard.tsx
+++ src/components/Recard.tsx
@@ -1,15 +1,21 @@
 import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
 
+import Latex from 'react-latex'
+import '../assets/katex.min.css'
+
 export type RecardProps = {
   question: string
   answer: string
+  displayMode?: boolean
 }
 
-export const Recard = ({ question, answer }: RecardProps): JSX.Element => {
+export const Recard = ({ question, answer, displayMode }: RecardProps): JSX.Element => {
   return (
     <Accordion>
       <AccordionSummary>{question}</AccordionSummary>
-      <AccordionDetails>{answer}</AccordionDetails>
+      <AccordionDetails>
+        <Latex displayMode={displayMode}>{answer}</Latex>
+      </AccordionDetails>
     </Accordion>
   )
 }
