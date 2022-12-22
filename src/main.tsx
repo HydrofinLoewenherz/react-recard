11dd8c2cb6487a71d591f0961032d57a5ea28f5a
cryeprecision <muench.rico@gmail.com>
2022-12-22 16:35:58 +0100

    Add basic theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { CssBaseline } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from './App'
 import './index.css'
+
+const theme = createTheme({ palette: { mode: 'dark' } })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
