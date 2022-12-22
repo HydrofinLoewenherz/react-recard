d820e47917d01c0df3483ee1af5fe2da0c84c2c6
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-22 18:54:46 +0100

    add deck list to home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,29 @@
-import { Recard } from '../components/Recard'
+import * as API from '../api/recard'
+import {useEffect, useState} from "react";
+import {Box, List, ListItem, Typography} from "@mui/material";
+import {useAuthStore} from "../store/auth";
 
 export const Home = () => {
-  return <h1>Welcome to recard</h1>
+
+  const [decks, setDecks] = useState<string[]>([])
+  useEffect(() => {
+    API.user().then((user) => {
+      setDecks(() => user?.decks || [])
+    })
+  }, [useAuthStore().username])
+
+  return (
+    <Box>
+      <Typography>
+        Home
+      </Typography>
+      <List>
+        {decks.map(deck =>
+          <ListItem>
+            {deck}
+          </ListItem>
+        )}
+      </List>
+    </Box>
+  )
 }
