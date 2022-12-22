b99c0e9851ca316cfb5fe5b2ac36e85c2e73d2f7
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-22 18:13:39 +0100

    implement encryption of local storage

--- src/api/recard/decks.ts
+++ src/api/recard/decks.ts
@@ -1,7 +1,42 @@
-import { Deck } from "./types";
+import {Deck} from "./types";
 import {useAuthStore} from "../../store/auth";
+import SHA256 from "crypto-js/sha256"
+import AES from "crypto-js/aes"
+import {isDev} from "../mode";
 
-// TODO encrypt with password (encrypt in prods)
+const useEncryption = !isDev()
+
+//<editor-fold desc="local storage access">
+const read = <T>(key: string): (T | null) => {
+  const authStore = useAuthStore()
+  if (!authStore.hasCredentials()) {
+    return null
+  }
+
+  const store = localStorage.getItem(`recard_user-${authStore.username}_${key}`)
+  if (store === null) {
+    return null
+  }
+
+  const decrypted = AES.decrypt(store, SHA256(authStore.password as string))
+  return JSON.parse(useEncryption ? decrypted.toString() : store)
+}
+const write = <T>(key: string, data: T | null): boolean => {
+  const authStore = useAuthStore()
+  if (!authStore.hasCredentials()) {
+    return false
+  }
+
+  if (data === null) {
+    localStorage.removeItem(`recard_user-${authStore.username}_${key}`)
+  } else {
+    const encrypted = AES.encrypt(JSON.stringify(data), SHA256(authStore.password as string))
+    localStorage.setItem(`recard_user-${authStore.username}_${key}`, useEncryption ? encrypted.toString() : JSON.stringify(data))
+  }
+  return true
+}
+//</editor-fold>
+
 
 interface UserData {
   username: string,
@@ -13,40 +48,22 @@ const EmptyUserData = (username: string): UserData => ({
   decks: []
 })
 
-export const user = async (): Promise<UserData> => {
+export const user = async (): Promise<UserData | null> => {
   const authStore = useAuthStore()
-  if (!authStore.hasCredentials()) {
-    return Promise.reject("not authenticated")
+  if (authStore.username === null) {
+    return null
   }
-
-  const storage = localStorage.getItem(`recard_user_${authStore.username}`)
-  return storage === null ? EmptyUserData(authStore.username as string) : JSON.parse(storage)
+  return read(`data`) || EmptyUserData(authStore.username)
 }
 
 export const deck = async (name: string): Promise<Deck | null> => {
-  const authStore = useAuthStore()
-  if (!authStore.hasCredentials()) {
-    return Promise.reject("not authenticated")
-  }
-
-  const storage = localStorage.getItem(`recard_user_${authStore.username}_deck_${name}`)
-  return storage === null ? null : JSON.parse(storage)
+  return read(`deck-${name}`)
 }
 
 export const setDeck = async (deck: Deck): Promise<void> => {
-  const authStore = useAuthStore()
-  if (!authStore.hasCredentials()) {
-    return Promise.reject("not authenticated")
-  }
-
-  localStorage.setItem(`recard_user_${authStore.username}_deck_${deck.name}`, JSON.stringify(deck))
+  write(`deck-${deck.name}`, deck)
 }
 
 export const unsetDeck = async (name: string): Promise<void> => {
-  const authStore = useAuthStore()
-  if (!authStore.hasCredentials()) {
-    return Promise.reject("not authenticated")
-  }
-
-  localStorage.unsetItem(`recard_user_${authStore.username}_deck_${deck.name}`)
+  write(`deck-${name}`, null)
 }
