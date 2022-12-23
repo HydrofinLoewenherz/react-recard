6cb0b4bd017d00768ef9f9e35333b35ddbb06c3c
cryeprecision <muench.rico@gmail.com>
2022-12-23 12:23:29 +0100

    Remove styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
 import { Deck } from './routes/Deck'
 import { Error } from './routes/Error'
 import { Home } from './routes/Home'
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './index.css'
 
 const theme = createTheme({ palette: { mode: 'dark' } })
 
