4953229b429a182c9b39ead54d45f7d83436acda
cryeprecision <muench.rico@gmail.com>
2022-12-23 13:04:32 +0100

    Make `Login` interact with the store

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,12 +1,15 @@
-import { Box, Button, ButtonGroup, Checkbox, FormControlLabel, Stack, TextField, Typography } from '@mui/material'
+import { Box, ButtonGroup, Checkbox, FormControlLabel, Stack, TextField, Typography } from '@mui/material'
 import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'
+import { exists, isKnown, setKnown, useAuthStore } from '../store/auth'
+import { FormButton } from '../components/FormButton'
 
 export const Login = () => {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [stayLoggedIn, setStayLoggedIn] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
+  const store = useAuthStore()
 
   const clearInput = (): void => {
     setUsername('')
@@ -15,43 +18,65 @@ export const Login = () => {
   }
 
   const onSignup = () => {
+    if (exists(username)) {
+      alert('Username already exists')
+    } else {
+      setKnown({ username, password })
+      alert('Created user, you can login now')
+    }
     clearInput()
-    setError("Couldn't sign up")
-    alert("Couldn't sign up")
   }
   const onLogin = () => {
+    if (isKnown({ username, password })) {
+      store.setCredentials({ username, password }, stayLoggedIn)
+    } else {
+      alert!('Invalid username or password')
+    }
     clearInput()
-    setError('Invalid user')
-    alert('Invalid user')
+  }
+  const onLogout = () => {
+    store.unsetCredentials()
   }
 
-  const validInput = username !== '' && password !== ''
+  const validPassword = password !== '' && /\d{2,}/i.test(password)
+  const validInput = username !== '' && validPassword
+
+  const Login_ = () => (
+    <Stack gap={2}>
+      <Typography variant='h4' sx={{ mx: 'auto' }}>
+        Login
+      </Typography>
+      <TextField value={username} onChange={({ target }) => setUsername(target.value)} label='Username' />
+      <TextField value={password} onChange={({ target }) => setPassword(target.value)} label='Password' />
+      <ButtonGroup>
+        <FormButton onClick={onLogin} fullWidth disabled={!validInput}>
+          Login
+        </FormButton>
+        <FormButton onClick={onSignup} fullWidth disabled={!validInput}>
+          Signup
+        </FormButton>
+      </ButtonGroup>
+      <FormControlLabel
+        control={<Checkbox checked={stayLoggedIn} onChange={({ target }) => setStayLoggedIn(target.checked)} />}
+        label='Stay logged in'
+      />
+    </Stack>
+  )
+  const Logout_ = () => (
+    <Stack gap={2}>
+      <Typography variant='h4' sx={{ mx: 'auto' }}>
+        {`Hello, ${store.username}`}
+      </Typography>
+      <FormButton onClick={onLogout} fullWidth>
+        Logout
+      </FormButton>
+    </Stack>
+  )
 
   return (
     <Grid container direction='column' alignItems='center' justifyContent='center' sx={{ minHeight: '100vh' }}>
       <Grid xs={12} md={6} lg={3}>
-        <Box sx={{ p: 2 }}>
-          <Stack gap={2}>
-            <Typography variant='h4' sx={{ mx: 'auto' }}>
-              Login
-            </Typography>
-            <TextField value={username} onChange={({ target }) => setUsername(target.value)} label='Username' />
-            <TextField value={password} onChange={({ target }) => setPassword(target.value)} label='Password' />
-            <ButtonGroup>
-              <Button onClick={onLogin} fullWidth disabled={!validInput}>
-                Login
-              </Button>
-              <Button onClick={onSignup} fullWidth disabled={!validInput}>
-                Signup
-              </Button>
-            </ButtonGroup>
-            <FormControlLabel
-              control={<Checkbox checked={stayLoggedIn} onChange={({ target }) => setStayLoggedIn(target.checked)} />}
-              label='Stay logged in'
-            />
-          </Stack>
-          <Stack gap={0}></Stack>
-        </Box>
+        <Box sx={{ p: 2 }}>{(store.hasCredentials() && Logout_()) || Login_()}</Box>
       </Grid>
     </Grid>
   )
