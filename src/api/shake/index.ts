6b2c2090c269a04d82b3a519bbde9676ad2c38d4
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-23 13:56:08 +0100

    add debounce to shake options adn docu

--- src/api/shake/index.ts
+++ src/api/shake/index.ts
@@ -1,51 +1,27 @@
+/** Options for registering a shake handler. */
 export interface ShakeOptions {
+  /** ... (in m/s^2) */
   magThreshold: number
+  /** ... (in ms) */
+  debounce: number
 }
 
 const DefaultShakeOptions: ShakeOptions = {
-  magThreshold: 15
+  magThreshold: 10,
+  debounce: 150,
 }
 
+/** The context in witch a shake happens, hods metadata about the shake. */
 export interface ShakeHandlerContext {
   options: ShakeOptions,
   motion: DeviceMotionEvent
 }
 
-/** Handles any shake event,  */
-export type ShakeHandler = (ctx: ShakeHandlerContext) => (boolean | void)
+/** Handles any shake event, with given context of the shake. */
+export type ShakeHandler = (ctx: ShakeHandlerContext) => void
 
-// register global listener for all handlers
-const handlers: Record<string, { handler: ShakeHandler, options: ShakeOptions }> = {}
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('devicemotion', (ev) => {
-    if (ev.acceleration === null) {
-      return
-    }
-
-    Object.entries(handlers)
-      .forEach(([key, { handler, options }]) => {
-        const acc = ev.acceleration
-        const acc_x = acc!.x || 0
-        const acc_y = acc!.y || 0
-        const acc_z = acc!.z || 0
-
-        const mag = Math.sqrt((acc_x * acc_x) + (acc_y * acc_y) + (acc_z * acc_z));
-        if (mag > options.magThreshold) {
-          console.debug(`detected shake with mag ${mag}`)
-          if (handler({
-            options: options,
-            motion: ev
-          })) {
-            console.debug(`stopping shake listen ${key}`)
-            delete handlers[key]
-          }
-        }
-    })
-  })
-}
-
-
-export function onShake(handler: ShakeHandler, options?: Partial<ShakeOptions>): boolean {
+/** Registers a handler for a shake event. Returns 'false' if not supported or a function to stop the event listener. */
+export function onShake(handler: ShakeHandler, options?: Partial<ShakeOptions>) {
   if (!window.DeviceOrientationEvent) {
     return false
   }
@@ -53,11 +29,30 @@ export function onShake(handler: ShakeHandler, options?: Partial<ShakeOptions>):
     ...DefaultShakeOptions,
     ...(options || {}),
   }
-  const key = (Math.random() + 1).toString(36).substring(7)
-  console.debug(`adding shake handler as ${key}`)
-  handlers[key] = {
-    handler,
-    options: completeOptions
+  let lastCallTime = new Date().getTime()
+
+  const handle = (ev: DeviceMotionEvent) => {
+    const acc = ev.acceleration
+    if (acc === null) {
+      return
+    }
+
+    const acc_x = acc.x || 0
+    const acc_y = acc.y || 0
+    const acc_z = acc.z || 0
+    const mag = Math.sqrt((acc_x * acc_x) + (acc_y * acc_y) + (acc_z * acc_z));
+
+    const currTime = new Date().getTime()
+    if (mag > completeOptions.magThreshold && currTime - lastCallTime > completeOptions.debounce) {
+      console.debug(`detected shake with mag ${mag}`, ev.acceleration)
+      lastCallTime = new Date().getTime()
+      handler({
+        options: completeOptions,
+        motion: ev
+      })
+    }
   }
-  return true
+  window.addEventListener('devicemotion', handle)
+
+  return () => window.removeEventListener('devicemotion', handle)
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -13,22 +13,18 @@ export const Home = () => {
     })
   }, [useAuthStore().username])
 
-  const [isListening, setListening] = useState(false)
-  const [shaken, setShaken] = useState(false)
   useEffect(() => {
     console.log('starting shake listener')
-    const listening = onShake(() => {
-      setShaken(true)
-    })
-    console.log(`shake listener started`, listening)
-    setListening(listening)
-  })
+    const handle = onShake(() => {})
+    console.log(`shake listener started`, handle !== false)
+    return () => {
+      console.log(`shake listener stopped`, handle !== false)
+      if (handle !== false) handle()
+    }
+  }, [])
 
   return (
     <Box>
-      <pre>
-        '{isListening}' '{shaken}'
-      </pre>
       <Typography>
         Home
       </Typography>
