26bc4a6cbd948348377b08c1abd886666b438766
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-28 15:20:07 +0100

    let recard open be controlled programmatically

--- src/components/Recard.tsx
+++ src/components/Recard.tsx
@@ -2,16 +2,22 @@ import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
 
 import Latex from 'react-latex'
 import '../assets/katex.min.css'
+import React from "react";
 
 export type RecardProps = {
   question: string
   answer: string
   displayMode?: boolean
+  showAnswer?: boolean
+  onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
-export const Recard = ({ question, answer, displayMode }: RecardProps): JSX.Element => {
+export const Recard = ({ question, answer, displayMode, showAnswer, onClick }: RecardProps): JSX.Element => {
   return (
-    <Accordion>
+    <Accordion
+      expanded={showAnswer}
+      onClick={onClick}
+    >
       <AccordionSummary>{question}</AccordionSummary>
       <AccordionDetails>
         <Latex displayMode={displayMode}>{answer}</Latex>
