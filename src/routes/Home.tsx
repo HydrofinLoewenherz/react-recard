bac371ffd5fba772173db1c9101f8803c2635e37
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-28 19:27:33 +0100

    use mui/cards to show decks

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,21 @@
-import {Box, Button, Paper, Stack, TextField, Typography} from '@mui/material'
-import { useState } from 'react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card, CardActions,
+  CardContent, CardHeader,
+  Container, IconButton, ListItemIcon, ListItemText, Menu, MenuItem,
+  Paper,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material'
+import { MoreVert, Delete, Edit } from '@mui/icons-material';
+import React, { useState } from 'react'
 import { FormButton } from '../components'
 import { useStore } from '../store/store'
 import { Deck } from '../types'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type DeckInfoProps = {
   deck: Deck
@@ -11,18 +23,63 @@ type DeckInfoProps = {
 const DeckInfo = ({ deck }: DeckInfoProps) => {
   const removeDeck = useStore(store => store.removeDeck)
 
-  const onDelete = async () => {
-    if (!removeDeck(deck.name)) {
-      alert("Couldn't remove deck")
-    }
-  }
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <Paper elevation={1}>
-      <Typography>{deck.name}</Typography>
-      <FormButton onClick={onDelete}>Delete</FormButton>
-      <Button variant='outlined' component={Link} to={`/learn/${deck.name}`}>Learn</Button>
-    </Paper>
+    <Card>
+      <CardHeader
+        action={
+          <>
+            <IconButton
+              onClick={handleClick}
+            >
+              <MoreVert />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem
+                component={Link}
+                to={`/deck/${deck.name}`}
+              >
+                <ListItemIcon>
+                  <Edit />
+                </ListItemIcon>
+                <ListItemText>
+                  Manage
+                </ListItemText>
+              </MenuItem>
+              <MenuItem
+                onClick={() => removeDeck(deck.name)}
+              >
+                <ListItemIcon>
+                  <Delete />
+                </ListItemIcon>
+                <ListItemText>
+                  Delete
+                </ListItemText>
+              </MenuItem>
+            </Menu>
+          </>
+        }
+        title={deck.name}
+      />
+      <CardContent>
+        Stats!
+      </CardContent>
+      <CardActions>
+        <Button variant='outlined' component={Link} to={`/learn/${deck.name}`}>Learn</Button>
+      </CardActions>
+    </Card>
   )
 }
 
@@ -34,7 +91,7 @@ const AddDeck = () => {
 
   const onSave = async () => {
     if (!setDeck({ name, cards: [] })) {
-      alert("Coulnd't save deck")
+      alert("Couldn't save deck")
       return
     }
     if (!(await saveDecks())) {
@@ -55,9 +112,11 @@ export const Home = () => {
   const decks = useStore(store => store.decks)
 
   return (
-    <Box>
+    <Container>
       <AddDeck />
-      <Stack gap={1}>{decks !== null && decks.map((deck, i) => <DeckInfo deck={deck} key={i} />)}</Stack>
-    </Box>
+      <Stack gap={1}>
+        {decks !== null && decks.map((deck, i) => <DeckInfo deck={deck} key={i} />)}
+      </Stack>
+    </Container>
   )
 }
