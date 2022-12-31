9755dc3f8e2b86133afdec24183aa56a34ec234a
Paul Johannes Wagner <hydrofin@gmail.com>
2022-12-31 16:00:42 +0100

    add basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
-import { Box } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
-  return <Box mt={'auto'}></Box>
+  return (
+    <Box position={'fixed'} bottom={0} width={'100%'}>
+      <Paper sx={{ p: 2 }} square>
+        <Typography>
+          Created by <Link to={'https://github.com/cryeprecision'}>Rico (----)</Link> and{' '}
+          <Link to={'https://github.com/HydrofinLoewenherz'}>Paul (ujhtl)</Link>
+        </Typography>
+      </Paper>
+    </Box>
+  )
 }
